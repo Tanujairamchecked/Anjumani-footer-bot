3f66b9b724c86a90ffa22c696948df7e1ece65ce
Tanujairamchecked <94793933+Tanujairamchecked@users.noreply.github.com>
2021-11-27 21:27:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ async def _start(bot: Client, m: Message):
             Config.START_TEXT,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("Support Group", url="https://t.me/DevsZone"), InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")],
-                    [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")]
+                    [InlineKeyboardButton("Channel", url="https://t.me/anjumani_zone"), InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")],
+                    [InlineKeyboardButton("Group", url="https://t.me/+E-6DtQ6YTjAxNDI1")]
                 ]
             ),
             quote=True
