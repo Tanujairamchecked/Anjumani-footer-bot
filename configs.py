734440dea945ad51504fd37780e86e1ba8005ac8
Tanujairamchecked <94793933+Tanujairamchecked@users.noreply.github.com>
2023-04-02 19:59:48 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("API_ID", 1234567))
     API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5575527838:AAFTFsaENJdgfSeQaVs4rREL1Hmgu3IQPdM")
     SESSION_NAME = os.environ.get("SESSION_NAME", "Footer-Bot")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
